171a2526e9178a4ac4beccedaf590cebe0976650
htetz04 <simontun.zlh@gmail.com>
2024-12-01 15:39:13 -0500

    byte update

--- USB_Buffer/source/USB_Buffer.sv
+++ USB_Buffer/source/USB_Buffer.sv
@@ -58,30 +58,203 @@ module USB_Buffer(
                 queue_DB = 1'b1;
             end
 
-            if(Buffer_Occupancy == 7'd0) begin
-                n_DB[7:0] = new_byte;
-            end
-            else if(Buffer_Occupancy == 7'd1) begin
-                n_DB[15:8] = new_byte;
-            end
-            else if(Buffer_Occupancy == 7'd2) begin
-                n_DB[23:16] = new_byte;
-            end
-            else if(Buffer_Occupancy == 7'd3) begin
-                n_DB[31:24] = new_byte;
-            end
-            else if(Buffer_Occupancy == 7'd4) begin
-                n_DB[39:32] = new_byte;
-            end
-            else if(Buffer_Occupancy == 7'd5) begin
-                n_DB[47:40] = new_byte;
-            end
-            else if(Buffer_Occupancy == 7'd6) begin
-                n_DB[55:48] = new_byte;
-            end
-            else if(Buffer_Occupancy == 7'd7) begin
-                n_DB[63:56] = new_byte;
-            end
+            case(Buffer_Occupancy) 
+                7'd0: begin
+                    n_DB[7:0] = new_byte;
+                end
+                7'd1: begin
+                    n_DB[15:8] = new_byte;
+                end
+                7'd2: begin
+                    n_DB[23:16] = new_byte;
+                end
+                7'd3: begin
+                    n_DB[31:24] = new_byte;
+                end
+                7'd4: begin
+                    n_DB[39:32] = new_byte;
+                end
+                7'd5: begin
+                    n_DB[47:40] = new_byte;
+                end
+                7'd6: begin
+                    n_DB[55:48] = new_byte;
+                end
+                7'd7: begin
+                    n_DB[63:56] = new_byte;
+                end
+                7'd8: begin
+                    n_DB[71:64] = new_byte;
+                end
+                7'd9: begin
+                    n_DB[79:72] = new_byte;
+                end
+                7'd10: begin
+                    n_DB[87:80] = new_byte;
+                end
+                7'd11: begin
+                    n_DB[95:88] = new_byte;
+                end
+                7'd12: begin
+                    n_DB[103:96] = new_byte;
+                end
+                7'd13: begin
+                    n_DB[111:104] = new_byte;
+                end
+                7'd14: begin
+                    n_DB[119:112] = new_byte;
+                end
+                7'd15: begin
+                    n_DB[127:120] = new_byte;
+                end
+                7'd16: begin
+                    n_DB[135:128] = new_byte;
+                end
+                7'd17: begin
+                    n_DB[143:136] = new_byte;
+                end
+                7'd18: begin
+                    n_DB[151:144] = new_byte;
+                end
+                7'd19: begin
+                    n_DB[159:152] = new_byte;
+                end
+                7'd20: begin
+                    n_DB[167:160] = new_byte;
+                end
+                7'd21: begin
+                    n_DB[175:168] = new_byte;
+                end
+                7'd22: begin
+                    n_DB[183:176] = new_byte;
+                end
+                7'd23: begin
+                    n_DB[191:184] = new_byte;
+                end
+                7'd24: begin
+                    n_DB[199:192] = new_byte;
+                end
+                7'd25: begin
+                    n_DB[207:200] = new_byte;
+                end
+                7'd26: begin
+                    n_DB[215:208] = new_byte;
+                end
+                7'd27: begin
+                    n_DB[223:216] = new_byte;
+                end
+                7'd28: begin
+                    n_DB[231:224] = new_byte;
+                end
+                7'd29: begin
+                    n_DB[239:232] = new_byte;
+                end
+                7'd30: begin
+                    n_DB[247:240] = new_byte;
+                end
+                7'd31: begin
+                    n_DB[255:248] = new_byte;
+                end
+                7'd32: begin
+                    n_DB[263:256] = new_byte;
+                end
+                7'd33: begin
+                    n_DB[271:264] = new_byte;
+                end
+                7'd34: begin
+                    n_DB[279:272] = new_byte;
+                end
+                7'd35: begin
+                    n_DB[287:280] = new_byte;
+                end
+                7'd36: begin
+                    n_DB[295:288] = new_byte;
+                end
+                7'd37: begin
+                    n_DB[303:296] = new_byte;
+                end
+                7'd38: begin
+                    n_DB[311:304] = new_byte;
+                end
+                7'd39: begin
+                    n_DB[319:312] = new_byte;
+                end
+                7'd40: begin
+                    n_DB[327:320] = new_byte;
+                end
+                7'd41: begin
+                    n_DB[335:328] = new_byte;
+                end
+                7'd42: begin
+                    n_DB[343:336] = new_byte;
+                end
+                7'd43: begin
+                    n_DB[351:344] = new_byte;
+                end
+                7'd44: begin
+                    n_DB[359:352] = new_byte;
+                end
+                7'd45: begin
+                    n_DB[367:360] = new_byte;
+                end
+                7'd46: begin
+                    n_DB[375:368] = new_byte;
+                end
+                7'd47: begin
+                    n_DB[383:376] = new_byte;
+                end
+                7'd48: begin
+                    n_DB[391:384] = new_byte;
+                end
+                7'd49: begin
+                    n_DB[399:392] = new_byte;
+                end
+                7'd50: begin
+                    n_DB[407:400] = new_byte;
+                end
+                7'd51: begin
+                    n_DB[415:408] = new_byte;
+                end
+                7'd52: begin
+                    n_DB[423:416] = new_byte;
+                end
+                7'd53: begin
+                    n_DB[431:424] = new_byte;
+                end
+                7'd54: begin
+                    n_DB[439:432] = new_byte;
+                end
+                7'd55: begin
+                    n_DB[447:440] = new_byte;
+                end
+                7'd56: begin
+                    n_DB[455:448] = new_byte;
+                end
+                7'd57: begin
+                    n_DB[463:456] = new_byte;
+                end
+                7'd58: begin
+                    n_DB[471:464] = new_byte;
+                end
+                7'd59: begin
+                    n_DB[479:472] = new_byte;
+                end
+                7'd60: begin
+                    n_DB[487:480] = new_byte;
+                end
+                7'd61: begin
+                    n_DB[495:488] = new_byte;
+                end
+                7'd62: begin
+                    n_DB[503:496] = new_byte;
+                end
+                7'd63: begin
+                    n_DB[511:504] = new_byte;
+                end
+                7'd64: begin
+                    n_DB[519:512] = new_byte;
+                end
+            endcase
         end
         
     end
--- USB_TX/source/USB_TX_Packet_Compiler.sv
+++ USB_TX/source/USB_TX_Packet_Compiler.sv
@@ -102,7 +102,189 @@ module USB_TX_Packet_Compiler(
                     8'd5: begin
                         n_packet_TX[55:48] = TX_Packet_Data_Buffer;
                     end
-
+                    8'd6: begin
+                        n_packet_TX[63:56] = TX_Packet_Data_Buffer;
+                    end
+                    8'd7: begin
+                        n_packet_TX[71:64] = TX_Packet_Data_Buffer;
+                    end
+                    8'd8: begin
+                        n_packet_TX[79:72] = TX_Packet_Data_Buffer;
+                    end
+                    8'd9: begin
+                        n_packet_TX[87:80] = TX_Packet_Data_Buffer;
+                    end
+                    8'd10: begin
+                        n_packet_TX[95:88] = TX_Packet_Data_Buffer;
+                    end
+                    8'd11: begin
+                        n_packet_TX[103:96] = TX_Packet_Data_Buffer;
+                    end
+                    8'd12: begin
+                        n_packet_TX[111:104] = TX_Packet_Data_Buffer;
+                    end
+                    8'd13: begin
+                        n_packet_TX[119:112] = TX_Packet_Data_Buffer;
+                    end
+                    8'd14: begin
+                        n_packet_TX[127:120] = TX_Packet_Data_Buffer;
+                    end
+                    8'd15: begin
+                        n_packet_TX[135:128] = TX_Packet_Data_Buffer;
+                    end
+                    8'd16: begin
+                        n_packet_TX[143:136] = TX_Packet_Data_Buffer;
+                    end
+                    8'd17: begin
+                        n_packet_TX[151:144] = TX_Packet_Data_Buffer;
+                    end
+                    8'd18: begin
+                        n_packet_TX[159:152] = TX_Packet_Data_Buffer;
+                    end
+                    8'd19: begin
+                        n_packet_TX[167:160] = TX_Packet_Data_Buffer;
+                    end
+                    8'd20: begin
+                        n_packet_TX[175:168] = TX_Packet_Data_Buffer;
+                    end
+                    8'd21: begin
+                        n_packet_TX[183:176] = TX_Packet_Data_Buffer;
+                    end
+                    8'd22: begin
+                        n_packet_TX[191:184] = TX_Packet_Data_Buffer;
+                    end
+                    8'd23: begin
+                        n_packet_TX[199:192] = TX_Packet_Data_Buffer;
+                    end
+                    8'd24: begin
+                        n_packet_TX[207:200] = TX_Packet_Data_Buffer;
+                    end
+                    8'd25: begin
+                        n_packet_TX[215:208] = TX_Packet_Data_Buffer;
+                    end
+                    8'd26: begin
+                        n_packet_TX[223:216] = TX_Packet_Data_Buffer;
+                    end
+                    8'd27: begin
+                        n_packet_TX[231:224] = TX_Packet_Data_Buffer;
+                    end
+                    8'd28: begin
+                        n_packet_TX[239:232] = TX_Packet_Data_Buffer;
+                    end
+                    8'd29: begin
+                        n_packet_TX[247:240] = TX_Packet_Data_Buffer;
+                    end
+                    8'd30: begin
+                        n_packet_TX[255:248] = TX_Packet_Data_Buffer;
+                    end
+                    8'd31: begin
+                        n_packet_TX[263:256] = TX_Packet_Data_Buffer;
+                    end
+                    8'd32: begin
+                        n_packet_TX[271:264] = TX_Packet_Data_Buffer;
+                    end
+                    8'd33: begin
+                        n_packet_TX[279:272] = TX_Packet_Data_Buffer;
+                    end
+                    8'd34: begin
+                        n_packet_TX[287:280] = TX_Packet_Data_Buffer;
+                    end
+                    8'd35: begin
+                        n_packet_TX[295:288] = TX_Packet_Data_Buffer;
+                    end
+                    8'd36: begin
+                        n_packet_TX[303:296] = TX_Packet_Data_Buffer;
+                    end
+                    8'd37: begin
+                        n_packet_TX[311:304] = TX_Packet_Data_Buffer;
+                    end
+                    8'd38: begin
+                        n_packet_TX[319:312] = TX_Packet_Data_Buffer;
+                    end
+                    8'd39: begin
+                        n_packet_TX[327:320] = TX_Packet_Data_Buffer;
+                    end
+                    8'd40: begin
+                        n_packet_TX[335:328] = TX_Packet_Data_Buffer;
+                    end
+                    8'd41: begin
+                        n_packet_TX[343:336] = TX_Packet_Data_Buffer;
+                    end
+                    8'd42: begin
+                        n_packet_TX[351:344] = TX_Packet_Data_Buffer;
+                    end
+                    8'd43: begin
+                        n_packet_TX[359:352] = TX_Packet_Data_Buffer;
+                    end
+                    8'd44: begin
+                        n_packet_TX[367:360] = TX_Packet_Data_Buffer;
+                    end
+                    8'd45: begin
+                        n_packet_TX[375:368] = TX_Packet_Data_Buffer;
+                    end
+                    8'd46: begin
+                        n_packet_TX[383:376] = TX_Packet_Data_Buffer;
+                    end
+                    8'd47: begin
+                        n_packet_TX[391:384] = TX_Packet_Data_Buffer;
+                    end
+                    8'd48: begin
+                        n_packet_TX[399:392] = TX_Packet_Data_Buffer;
+                    end
+                    8'd49: begin
+                        n_packet_TX[407:400] = TX_Packet_Data_Buffer;
+                    end
+                    8'd50: begin
+                        n_packet_TX[415:408] = TX_Packet_Data_Buffer;
+                    end
+                    8'd51: begin
+                        n_packet_TX[423:416] = TX_Packet_Data_Buffer;
+                    end
+                    8'd52: begin
+                        n_packet_TX[431:424] = TX_Packet_Data_Buffer;
+                    end
+                    8'd53: begin
+                        n_packet_TX[439:432] = TX_Packet_Data_Buffer;
+                    end
+                    8'd54: begin
+                        n_packet_TX[447:440] = TX_Packet_Data_Buffer;
+                    end
+                    8'd55: begin
+                        n_packet_TX[455:448] = TX_Packet_Data_Buffer;
+                    end
+                    8'd56: begin
+                        n_packet_TX[463:456] = TX_Packet_Data_Buffer;
+                    end
+                    8'd57: begin
+                        n_packet_TX[471:464] = TX_Packet_Data_Buffer;
+                    end
+                    8'd58: begin
+                        n_packet_TX[479:472] = TX_Packet_Data_Buffer;
+                    end
+                    8'd59: begin
+                        n_packet_TX[487:480] = TX_Packet_Data_Buffer;
+                    end
+                    8'd60: begin
+                        n_packet_TX[495:488] = TX_Packet_Data_Buffer;
+                    end
+                    8'd61: begin
+                        n_packet_TX[503:496] = TX_Packet_Data_Buffer;
+                    end
+                    8'd62: begin
+                        n_packet_TX[511:504] = TX_Packet_Data_Buffer;
+                    end
+                    8'd63: begin
+                        n_packet_TX[519:512] = TX_Packet_Data_Buffer;
+                    end
+                    8'd64: begin
+                        n_packet_TX[527:520] = TX_Packet_Data_Buffer;
+                    end
+                    8'd65: begin
+                        n_packet_TX[535:528] = TX_Packet_Data_Buffer;
+                    end
+                    8'd66: begin
+                        n_packet_TX[543:536] = TX_Packet_Data_Buffer;
+                    end
                 endcase
 
                 if(Buffer_Occupancy != 0) begin
